c677a8b10c8364dd2238c0c71f08ba486971a3d1
Robert McGibbon <rmcgibbo@gmail.com>
2014-05-17 18:10:19 -0700

    nicer formatting for list_articles

---
+++ figshare/utils.py
@@ -0,0 +1,14 @@
+from six.moves.html_parser import HTMLParser
+
+def strip_html(html):
+    class MLStripper(HTMLParser):
+        def __init__(self):
+            self.reset()
+            self.fed = []
+        def handle_data(self, d):
+            self.fed.append(d)
+        def get_data(self):
+            return ''.join(self.fed)
+    p = MLStripper()
+    p.feed(html)
+    return p.get_data()
